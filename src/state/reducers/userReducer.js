e9a710bd2faecf21c8167d9f69c4216034cdb8f4
Sophia Sagan <sophiabraddock82@gmail>
2020-10-13 10:19:47 -0400

    Redux implemented with user functions

---
+++ src/state/reducers/userReducer.js
@@ -0,0 +1,23 @@
+import { INITIALIZE_USER } from '../actions/userActionTypes';
+
+const initialState = {
+  initialized: false,
+  okta_uid: null,
+  email: null,
+};
+
+const userReducer = (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case INITIALIZE_USER:
+      for (let key in action.payload) {
+        newState[key] = action.payload[key];
+      }
+      break;
+    default:
+      break;
+  }
+  return newState;
+};
+
+export default userReducer;
