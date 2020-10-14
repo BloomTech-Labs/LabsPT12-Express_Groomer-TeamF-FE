5e7059dac94fc925353ba45aeca5683cd7c81d1b
David White <dwhite0627@gmail.com>
2020-10-13 22:02:40 -0400

    In the url variable, refer to the environment variable REACT_APP_API_URI

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const url =
-  process.env.REACT_APP_API_URL ||
+  process.env.REACT_APP_API_URI ||
   'https://labspt12-express-groomer-f-api.herokuapp.com';
 
 export const axiosWithAuth = () => {
