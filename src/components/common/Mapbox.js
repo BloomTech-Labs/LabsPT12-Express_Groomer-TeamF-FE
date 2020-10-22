6ed9c5a13cf919c0d0a4ac5723b4aa829e25db7e
Harry Merritt <harry@merritt.es>
2020-10-22 02:14:33 +0100

    display each groomer on the map as an icon
    - currently using dummy data; will eventually get actual array from redux store

--- src/components/common/Mapbox.js
+++ src/components/common/Mapbox.js
@@ -16,6 +16,47 @@ const Mapbox = () => {
     zoom: 10.3,
   });
 
+  // Groomers (eventually will get from redux)
+  const mapboxGroomers = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-77.034084142948, 38.909671288923],
+        },
+        properties: {
+          id: '1',
+          groomerId: 'ce68mjqz2by3t2peo6i4',
+          businessName: 'Furry Friends Grooming & Care',
+          address: '2081 Robin Stravenue Apt. 490',
+          email: 'Lelia77@gmail.com',
+          phoneNumber: '876-403-2046',
+          lat: 5.5918,
+          lng: -159.2544,
+        },
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-77.049766, 38.900772],
+        },
+        properties: {
+          id: '2',
+          groomerId: 'bftqzfqkq3xzj953q64r',
+          businessName: "Gillian's Fine Pet Grooming",
+          address: '7857 Runte Court Suite 257',
+          email: 'Adele77@hotmail.com',
+          phoneNumber: '725-266-0638',
+          lat: 13.4941,
+          lng: 129.8933,
+        },
+      },
+    ],
+  };
+
   useEffect(() => {
     // Init mapbox
     const map = new mapboxgl.Map({
@@ -23,6 +64,25 @@ const Mapbox = () => {
       style: 'mapbox://styles/express-groomer/ckgio5w452keg19mofhut68o7', // stylesheet location
       center: [mapboxView.lng, mapboxView.lat], // starting position [lng, lat]
       zoom: mapboxView.zoom, // starting zoom
+    });
+
+    // Load groomer profiles onto map
+    map.on('load', function(e) {
+      /* Add the data to your map as a layer */
+      map.addLayer({
+        id: 'locations',
+        type: 'symbol',
+        /* Add a GeoJSON source containing place coordinates and information. */
+        source: {
+          type: 'geojson',
+          data: mapboxGroomers,
+        },
+        layout: {
+          'icon-image': 'veterinary-15',
+          'icon-allow-overlap': true,
+          'icon-size': 1.6,
+        },
+      });
     });
 
     // Mapbox move listener
